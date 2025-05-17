b5c3843a75177f9cf54aa81db2754c029c6475b9
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-17 15:56:35 +0530

    Delete backend/test.js

--- backend/test.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 5000;
-
-app.get('/', (req, res) => res.send('Backend is working!'));
-
-app.listen(port, () => console.log(`Test server running on port ${port}`));
