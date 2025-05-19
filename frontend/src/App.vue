9000225cc2fb127eacb8edd0b7d7972bb228a261
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-19 18:16:33 +0530

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,24 @@
 <template>
-    <v-app>
+  <v-app>
+    <amplify-authenticator>
       <router-view />
-    </v-app>
-  </template>
-  
-  <script>
-  export default {
-    name: 'App'
-  };
-  </script>
-  
+    </amplify-authenticator>
+  </v-app>
+</template>
+
+<script>
+import '@aws-amplify/ui-vue';
+
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+/* Optional: Style overrides for Amplify Auth UI */
+amplify-authenticator {
+  display: block;
+  margin: auto;
+  max-width: 400px;
+}
+</style>
