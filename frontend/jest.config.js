90bebbfe51e9fe9cdb01e674e5fccfc2d2fb6198
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-19 18:24:29 +0530

    Update jest.config.js

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -5,16 +5,25 @@ module.exports = {
     '^.+\\.js$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!axios).+\\.js$',
+    '/node_modules/(?!axios|aws-amplify).+\\.js$',
   ],
   moduleFileExtensions: ['js', 'json', 'vue'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   testEnvironment: 'jsdom',
 
   // Add testMatch to find your tests in "tests/" folder
   testMatch: [
     '**/tests/**/*.spec.[jt]s?(x)',
-  ]
+  ],
+
+  // Optional: Add coverage configuration if needed
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!src/main.js', // Exclude entry point file from coverage
+    '!src/router.js', // Exclude router.js from coverage
+  ],
+  coverageDirectory: '<rootDir>/tests/unit/coverage/',
 };
