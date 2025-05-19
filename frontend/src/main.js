20c54f43307cee01fc8c4246087d221a60d6af9a
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-19 18:17:44 +0530

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,20 @@ import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import router from './router';
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+import '@aws-amplify/ui-vue/styles.css';
+import { applyPolyfills, defineCustomElements } from '@aws-amplify/ui-components/loader';
 
 Vue.config.productionTip = false;
+
+// Configure AWS Amplify
+Amplify.configure(awsExports);
+
+// Optional: Register Amplify UI components globally if needed
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
 
 new Vue({
   vuetify,
