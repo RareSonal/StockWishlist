022e35e7118fecdf93775b97da09589a1ad548a6
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-22 01:01:05 +0530

    Update router.js

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -16,7 +16,7 @@ const router = new Router({
     },
     { 
       path: '/wishlist', 
-      component: Wishlist, 
+      component: WishlistPage, 
       meta: { requiresAuth: true } // Wishlist route, requires authentication
     },
     { 
