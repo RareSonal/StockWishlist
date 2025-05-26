87b82f2ff116042a9a212f7ff391fb9524a03c23
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-26 23:14:00 +0530

    Create outputs.tf

---
+++ terraform/modules/cognito/outputs.tf
@@ -0,0 +1,9 @@
+output "user_pool_id" {
+  value = aws_cognito_user_pool.user_pool.id
+}
+output "client_id" {
+  value = aws_cognito_user_pool_client.user_pool_client.id
+}
+output "user_pool_arn" {
+  value = aws_cognito_user_pool.user_pool.arn
+}
