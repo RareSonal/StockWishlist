9523661a3ca8079dd4416a024dfb3fefe2657919
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-28 17:02:50 +0530

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,20 @@
 <template>
-<v-app>
-<router-view />
-</v-app>
+  <v-app>
+    <router-view />
+  </v-app>
 </template>
 
 <script>
 export default {
-name: 'App'
+  name: 'App',
 };
 </script>
 
 <style>
 /* Global styles or layout tweaks can go here */
 body {
-font-family: 'Roboto', sans-serif;
-background-color: #f5f5f5;
-margin: 0;
+  font-family: 'Roboto', sans-serif;
+  background-color: #f5f5f5;
+  margin: 0;
 }
 </style>
