1722794d18f33b1b9481f16dee2ac930433ca8b2
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-28 17:17:36 +0530

    Update outputs.tf

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -1,19 +1,19 @@
 output "lambda_invoke_arn" {
-description = "Invoke ARN for the Flask backend Lambda function"
-value = aws_lambda_function.flask_backend.invoke_arn
+  description = "Invoke ARN for the Flask backend Lambda function"
+  value       = aws_lambda_function.flask_backend.invoke_arn
 }
 
 output "lambda_function_name" {
-description = "Name of the Flask backend Lambda function"
-value = aws_lambda_function.flask_backend.function_name
+  description = "Name of the Flask backend Lambda function"
+  value       = aws_lambda_function.flask_backend.function_name
 }
 
 output "user_migration_lambda_arn" {
-description = "Invoke ARN for the user migration Lambda function"
-value = aws_lambda_function.user_migration.invoke_arn
+  description = "Invoke ARN for the user migration Lambda function"
+  value       = aws_lambda_function.user_migration.invoke_arn
 }
 
 output "user_migration_lambda_name" {
-description = "Name of the user migration Lambda function"
-value = aws_lambda_function.user_migration.function_name
+  description = "Name of the user migration Lambda function"
+  value       = aws_lambda_function.user_migration.function_name
 }
