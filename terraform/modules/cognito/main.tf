8b7a6bdc73b71befeabca1a62248d8f954f6980c
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-28 16:26:23 +0530

    Update main.tf

--- terraform/modules/cognito/main.tf
+++ terraform/modules/cognito/main.tf
@@ -1,9 +1,21 @@
 resource "aws_cognito_user_pool" "user_pool" {
-  name = "stockwishlist-user-pool"
+name = "stockwishlist-user-pool"
+
+lambda_config {
+user_migration = var.user_migration_lambda_arn
+}
+
 }
 
 resource "aws_cognito_user_pool_client" "user_pool_client" {
-  name            = "stockwishlist-client"
-  user_pool_id    = aws_cognito_user_pool.user_pool.id
-  generate_secret = false
+name = "stockwishlist-client"
+user_pool_id = aws_cognito_user_pool.user_pool.id
+generate_secret = false
+
+explicit_auth_flows = [
+"ALLOW_USER_PASSWORD_AUTH",
+"ALLOW_ADMIN_USER_PASSWORD_AUTH",
+"ALLOW_REFRESH_TOKEN_AUTH",
+"ALLOW_USER_SRP_AUTH"
+]
 }
