e45d4eda7382a1a757cabdcd48e212117bfe3a72
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-28 23:41:54 +0530

    Update bootstrap_resources.tf

--- bootstrap/bootstrap_resources.tf
+++ bootstrap/bootstrap_resources.tf
@@ -27,7 +27,7 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "terraform-lock-table"
+  name         = "raresonal-stockwishlist-terraform-lock-table"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
