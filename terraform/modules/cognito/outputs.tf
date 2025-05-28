be837c540486e9fe7498f48c45592f34d2677b38
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-28 16:27:47 +0530

    Update outputs.tf

--- terraform/modules/cognito/outputs.tf
+++ terraform/modules/cognito/outputs.tf
@@ -1,9 +1,14 @@
 output "user_pool_id" {
-  value = aws_cognito_user_pool.user_pool.id
+description = "ID of the Cognito User Pool"
+value = aws_cognito_user_pool.user_pool.id
 }
+
 output "client_id" {
-  value = aws_cognito_user_pool_client.user_pool_client.id
+description = "ID of the Cognito App Client"
+value = aws_cognito_user_pool_client.user_pool_client.id
 }
+
 output "user_pool_arn" {
-  value = aws_cognito_user_pool.user_pool.arn
+description = "ARN of the Cognito User Pool"
+value = aws_cognito_user_pool.user_pool.arn
 }
