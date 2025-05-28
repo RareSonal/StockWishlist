c8c403bb860aa65492a0ee82f1a9ecf2cf513aae
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-28 17:09:04 +0530

    Update main.tf

--- terraform/modules/amplify/main.tf
+++ terraform/modules/amplify/main.tf
@@ -1,32 +1,32 @@
 resource "aws_amplify_app" "frontend" {
-name = "stockwishlist-frontend"
-repository = var.repository_url
-oauth_token = var.github_oauth_token
+  name       = "stockwishlist-frontend"
+  repository = var.repository_url
+  oauth_token = var.github_oauth_token
 
-environment_variables = {
-AWS_REGION = var.region
-VUE_APP_API_URL = var.api_url
-VUE_APP_COGNITO_POOL_ID = var.cognito_user_pool_id
-VUE_APP_COGNITO_CLIENT_ID = var.cognito_client_id
-VUE_APP_COGNITO_REGION = var.region
-}
+  environment_variables = {
+    AWS_REGION              = var.region
+    VUE_APP_API_URL         = var.api_url
+    VUE_APP_COGNITO_POOL_ID = var.cognito_user_pool_id
+    VUE_APP_COGNITO_CLIENT_ID = var.cognito_client_id
+    VUE_APP_COGNITO_REGION  = var.region
+  }
 
-build_spec = <<-EOT
-version: 1.0
-frontend:
-phases:
-preBuild:
-commands:
-- yarn install
-build:
-commands:
-- yarn build
-artifacts:
-baseDirectory: /build
-files:
-- '**/*'
-cache:
-paths:
-- node_modules/**/*
-EOT
+  build_spec = <<-EOT
+    version: 1.0
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - yarn install
+        build:
+          commands:
+            - yarn build
+      artifacts:
+        baseDirectory: /build
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - node_modules/**/*
+  EOT
 }
