dd2621795e801c58acd5fbd6ef0524fe504aa06a
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-28 17:18:38 +0530

    Update variables.tf

--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -1,45 +1,45 @@
 variable "cognito_user_pool_arn" {
-description = "ARN of the Cognito User Pool"
-type = string
+  description = "ARN of the Cognito User Pool"
+  type        = string
 }
 
 variable "subnet_ids" {
-description = "List of subnet IDs for Lambda VPC configuration"
-type = list(string)
+  description = "List of subnet IDs for Lambda VPC configuration"
+  type        = list(string)
 }
 
 variable "lambda_sg_id" {
-description = "Security Group ID for the Lambda function"
-type = string
+  description = "Security Group ID for the Lambda function"
+  type        = string
 }
 
 variable "db_host" {
-description = "Database host address"
-type = string
+  description = "Database host address"
+  type        = string
 }
 
 variable "db_username" {
-description = "Username for connecting to the database"
-type = string
+  description = "Username for connecting to the database"
+  type        = string
 }
 
 variable "db_password" {
-description = "Password for connecting to the database"
-type = string
+  description = "Password for connecting to the database"
+  type        = string
 }
 
 variable "cognito_user_pool_id" {
-description = "ID of the Cognito User Pool"
-type = string
+  description = "ID of the Cognito User Pool"
+  type        = string
 }
 
 variable "cognito_client_id" {
-description = "Cognito App Client ID"
-type = string
+  description = "Cognito App Client ID"
+  type        = string
 }
 
 variable "region" {
-description = "AWS region to deploy resources"
-type = string
-default = "us-east-1"
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
 }
