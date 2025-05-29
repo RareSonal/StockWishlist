eea76cf9a6b4902f4db2bf0c82459a627a2fe6d3
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-30 00:18:18 +0530

    Update variables.tf

--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -1,8 +1,3 @@
-variable "cognito_user_pool_arn" {
-  description = "ARN of the Cognito User Pool"
-  type        = string
-}
-
 variable "subnet_ids" {
   description = "List of subnet IDs for Lambda VPC configuration"
   type        = list(string)
@@ -25,16 +20,6 @@ variable "db_username" {
 
 variable "db_password" {
   description = "Password for connecting to the database"
-  type        = string
-}
-
-variable "cognito_user_pool_id" {
-  description = "ID of the Cognito User Pool"
-  type        = string
-}
-
-variable "cognito_client_id" {
-  description = "Cognito App Client ID"
   type        = string
 }
 
