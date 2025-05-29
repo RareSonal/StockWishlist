97d9483042b2efee047508252e646c52f7157f99
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-29 16:55:15 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "stockwishlist-terraform-state"
+    bucket         = "raresonal-stockwishlist-terraform-state-bucket"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"
+    dynamodb_table = "raresonal-stockwishlist-terraform-lock-table"
     encrypt        = true
   }
 }
