1d697184ed74e353419cad4a89dc5652ba3c3621
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-31 18:33:54 +0530

    Update outputs.tf

--- terraform/modules/ssm_parameters/outputs.tf
+++ terraform/modules/ssm_parameters/outputs.tf
@@ -2,3 +2,9 @@ output "db_username" {
   value     = aws_ssm_parameter.db_username.value
   sensitive = true
 }
+
+output "ssm_db_username" {
+  value     = aws_ssm_parameter.db_username.name
+  sensitive = true
+}
+
