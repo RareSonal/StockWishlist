8fc8bc9985dc677f593532bc823b0ba3a01dff8a
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-31 23:24:04 +0530

    Update variables.tf

--- terraform/modules/cognito/variables.tf
+++ terraform/modules/cognito/variables.tf
@@ -2,3 +2,7 @@ variable "user_migration_lambda_arn" {
   description = "ARN of the Lambda function used for Cognito user migration"
   type        = string
 }
+
+variable "region" {
+  type = string
+}
