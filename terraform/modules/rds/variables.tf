2de69d7419a62d7a647b4b9bb03c25af51b7d286
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-31 17:22:37 +0530

    Update variables.tf

--- terraform/modules/rds/variables.tf
+++ terraform/modules/rds/variables.tf
@@ -6,9 +6,6 @@ variable "use_public_subnet_for_rds" {
 variable "public_subnet_ids" {
   type = list(string)
 }
-variable "private_subnet_ids" {
-  type = list(string)
-}
 variable "security_group_id" {
   type = string
 }
