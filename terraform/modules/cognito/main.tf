0a6c1304cbfbbce13960ecfefb389d15c0306d41
RareSonal <sonali.mandrupkar@gmail.com>
2025-05-31 23:23:47 +0530

    Update main.tf

--- terraform/modules/cognito/main.tf
+++ terraform/modules/cognito/main.tf
@@ -1,5 +1,10 @@
 resource "aws_cognito_user_pool" "user_pool" {
   name = "stockwishlist-user-pool"  
+
+  lambda_config {
+    user_migration = var.user_migration_lambda_arn
+  }
+
 }
 
 resource "aws_cognito_user_pool_client" "user_pool_client" {
@@ -14,3 +19,31 @@ resource "aws_cognito_user_pool_client" "user_pool_client" {
     "ALLOW_USER_SRP_AUTH"
   ]
 }
+
+resource "aws_cognito_user_pool" "user_pool" {
+  name = "stockwishlist-user-pool"  
+}
+
+resource "aws_cognito_user_pool_client" "user_pool_client" {
+  name           = "stockwishlist-client"
+  user_pool_id   = aws_cognito_user_pool.user_pool.id
+  generate_secret = false
+
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_ADMIN_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_SRP_AUTH"
+  ]
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_lambda_permission" "allow_cognito_invoke" {
+  statement_id  = "AllowExecutionFromCognito"
+  action        = "lambda:InvokeFunction"
+  function_name = var.user_migration_lambda_arn
+  principal     = "cognito-idp.amazonaws.com"
+  source_arn    = "arn:aws:cognito-idp:${var.region}:${data.aws_caller_identity.current.account_id}:userpool/${aws_cognito_user_pool.user_pool.id}"
+}
+
