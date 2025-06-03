f1d949f1afc1fe83b183dac9b84cc03113650d28
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-03 18:39:45 +0530

    Update outputs.tf

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -1,5 +1,5 @@
 output "lambda_invoke_arn" {
-  description = "Invoke ARN for the Flask backend Lambda function"
+  description = "Invoke ARN for the Flask backend Lambda function (used by API Gateway)"
   value       = aws_lambda_function.flask_backend.invoke_arn
 }
 
@@ -9,8 +9,8 @@ output "lambda_function_name" {
 }
 
 output "user_migration_lambda_arn" {
-  description = "Invoke ARN for the user migration Lambda function"
-  value       = aws_lambda_function.user_migration.invoke_arn
+  description = "Raw ARN for the user migration Lambda function (used by Cognito)"
+  value       = aws_lambda_function.user_migration.arn
 }
 
 output "user_migration_lambda_name" {
