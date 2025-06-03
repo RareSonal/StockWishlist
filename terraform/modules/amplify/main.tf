908625c28771530385b3e57425f4703061676fd2
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-03 20:53:03 +0530

    Update main.tf

--- terraform/modules/amplify/main.tf
+++ terraform/modules/amplify/main.tf
@@ -1,14 +1,14 @@
 resource "aws_amplify_app" "frontend" {
-  name       = "stockwishlist-frontend"
-  repository = var.repository_url
+  name        = "stockwishlist-frontend"
+  repository  = var.repository_url
   oauth_token = var.github_oauth_token
 
   environment_variables = {
-    AWS_REGION              = var.region
-    VUE_APP_API_URL         = var.api_url
-    VUE_APP_COGNITO_POOL_ID = var.cognito_user_pool_id
-    VUE_APP_COGNITO_CLIENT_ID = var.cognito_client_id
-    VUE_APP_COGNITO_REGION  = var.region
+    VUE_APP_REGION             = var.region
+    VUE_APP_API_URL            = var.api_url
+    VUE_APP_COGNITO_POOL_ID    = var.cognito_user_pool_id
+    VUE_APP_COGNITO_CLIENT_ID  = var.cognito_client_id
+    VUE_APP_COGNITO_REGION     = var.region
   }
 
   build_spec = <<-EOT
