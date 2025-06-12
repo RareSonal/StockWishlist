090121ee2731319a8eb3b2b951b49aaeb1c3a0dd
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-12 21:51:40 +0530

    Create main.tf

---
+++ terraform/modules/cors/main.tf
@@ -0,0 +1,51 @@
+resource "aws_api_gateway_method" "options" {
+  rest_api_id   = var.rest_api_id
+  resource_id   = var.resource_id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "mock" {
+  rest_api_id             = var.rest_api_id
+  resource_id             = var.resource_id
+  http_method             = aws_api_gateway_method.options.http_method
+  type                    = "MOCK"
+  integration_http_method = "OPTIONS"
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "response" {
+  rest_api_id = var.rest_api_id
+  resource_id = var.resource_id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "integration_response" {
+  rest_api_id = var.rest_api_id
+  resource_id = var.resource_id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = aws_api_gateway_method_response.response.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET,POST,PUT,DELETE'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  response_templates = {
+    "application/json" = ""
+  }
+}
