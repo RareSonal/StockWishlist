005e98d08ebfe23afcca1b61c91342146789069b
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-12 21:58:17 +0530

    Update outputs.tf

--- terraform/modules/api_gateway/outputs.tf
+++ terraform/modules/api_gateway/outputs.tf
@@ -1,3 +1,19 @@
 output "api_url" {
   value = aws_api_gateway_stage.api_stage.invoke_url
 }
+
+output "api_id" {
+  value = aws_api_gateway_rest_api.api.id
+}
+
+output "login_resource_id" {
+  value = aws_api_gateway_resource.login.id
+}
+
+output "stocks_resource_id" {
+  value = aws_api_gateway_resource.stocks.id
+}
+
+output "wishlist_resource_id" {
+  value = aws_api_gateway_resource.wishlist.id
+}
