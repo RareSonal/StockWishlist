eef3109aa9d1cc13c22411c1ccda1c08eca1c63e
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-15 22:53:54 +0530

    Update outputs.tf

--- terraform/modules/api_gateway/outputs.tf
+++ terraform/modules/api_gateway/outputs.tf
@@ -1,11 +1,23 @@
-output "api_url" {
-  value = aws_api_gateway_stage.api_stage.invoke_url
-}
-
 output "api_id" {
   value = aws_api_gateway_rest_api.api.id
 }
 
+output "api_url" {
+  value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.region}.amazonaws.com/${var.stage_name}"
+}
+
 output "proxy_resource_id" {
   value = aws_api_gateway_resource.v1_proxy.id
+}
+
+output "login_resource_id" {
+  value = aws_api_gateway_resource.login.id
+}
+
+output "stocks_resource_id" {
+  value = aws_api_gateway_resource.stocks.id
+}
+
+output "wishlist_resource_id" {
+  value = aws_api_gateway_resource.wishlist.id
 }
