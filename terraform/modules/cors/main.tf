e4683377af697a6eeff7339a359cd424f1ade1b0
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-15 16:13:56 +0530

    Update main.tf

--- terraform/modules/cors/main.tf
+++ terraform/modules/cors/main.tf
@@ -1,31 +1,33 @@
+
 resource "aws_api_gateway_method" "options" {
-  rest_api_id   = var.rest_api_id
-  resource_id   = var.resource_id
-  http_method   = "OPTIONS"
-  authorization = "NONE"
-  # No API key required for OPTIONS requests
-  api_key_required = false
+  count             = var.create_options_method ? 1 : 0
+  rest_api_id       = var.rest_api_id
+  resource_id       = var.resource_id
+  http_method       = "OPTIONS"
+  authorization     = "NONE"
+  api_key_required  = false
 }
 
 resource "aws_api_gateway_integration" "mock" {
-  rest_api_id             = var.rest_api_id
-  resource_id             = var.resource_id
-  http_method             = aws_api_gateway_method.options.http_method
-  type                    = "MOCK"
-  integration_http_method = "OPTIONS"
+  count                     = var.create_options_method ? 1 : 0
+  rest_api_id               = var.rest_api_id
+  resource_id               = var.resource_id
+  http_method               = aws_api_gateway_method.options[0].http_method
+  type                      = "MOCK"
+  integration_http_method   = "OPTIONS"
   request_templates = {
     "application/json" = "{\"statusCode\": 200}"
   }
 }
 
 resource "aws_api_gateway_method_response" "response" {
+  count       = var.create_options_method ? 1 : 0
   rest_api_id = var.rest_api_id
   resource_id = var.resource_id
-  http_method = aws_api_gateway_method.options.http_method
+  http_method = aws_api_gateway_method.options[0].http_method
   status_code = "200"
 
   response_parameters = {
-    # These headers must be true so you can reference them in integration response
     "method.response.header.Access-Control-Allow-Headers" = true
     "method.response.header.Access-Control-Allow-Methods" = true
     "method.response.header.Access-Control-Allow-Origin"  = true
@@ -37,13 +39,13 @@ resource "aws_api_gateway_method_response" "response" {
 }
 
 resource "aws_api_gateway_integration_response" "integration_response" {
+  count       = var.create_options_method ? 1 : 0
   rest_api_id = var.rest_api_id
   resource_id = var.resource_id
-  http_method = aws_api_gateway_method.options.http_method
-  status_code = aws_api_gateway_method_response.response.status_code
+  http_method = aws_api_gateway_method.options[0].http_method
+  status_code = aws_api_gateway_method_response.response[0].status_code
 
   response_parameters = {
-    # Include all methods you support on this resource in Allow-Methods
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token'"
     "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET,POST,PUT,DELETE,PATCH'"
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
