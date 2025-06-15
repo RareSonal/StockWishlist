950308ed1baa08955173a5da0ab9e00a38c9739c
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-15 22:53:36 +0530

    Update main.tf

--- terraform/modules/cors/main.tf
+++ terraform/modules/cors/main.tf
@@ -1,25 +1,27 @@
+
+
 resource "aws_api_gateway_method" "options" {
-  count            = var.create_options_method ? 1 : 0
-  rest_api_id      = var.rest_api_id
-  resource_id      = var.resource_id
-  http_method      = "OPTIONS"
-  authorization    = "NONE"
-  api_key_required = false
+  count         = var.create_options_method ? 1 : 0
+  rest_api_id   = var.rest_api_id
+  resource_id   = var.resource_id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "mock" {
-  count                     = var.create_options_method ? 1 : 0
-  rest_api_id               = var.rest_api_id
-  resource_id               = var.resource_id
-  http_method               = aws_api_gateway_method.options[0].http_method
-  type                      = "MOCK"
-  integration_http_method   = "OPTIONS"
+resource "aws_api_gateway_integration" "options_mock" {
+  count                   = var.create_options_method ? 1 : 0
+  rest_api_id             = var.rest_api_id
+  resource_id             = var.resource_id
+  http_method             = aws_api_gateway_method.options[0].http_method
+  type                    = "MOCK"
+  integration_http_method = "OPTIONS"
+
   request_templates = {
     "application/json" = "{\"statusCode\": 200}"
   }
 }
 
-resource "aws_api_gateway_method_response" "response" {
+resource "aws_api_gateway_method_response" "options_response" {
   count       = var.create_options_method ? 1 : 0
   rest_api_id = var.rest_api_id
   resource_id = var.resource_id
@@ -37,20 +39,16 @@ resource "aws_api_gateway_method_response" "response" {
   }
 }
 
-resource "aws_api_gateway_integration_response" "integration_response" {
+resource "aws_api_gateway_integration_response" "options_integration_response" {
   count       = var.create_options_method ? 1 : 0
   rest_api_id = var.rest_api_id
   resource_id = var.resource_id
   http_method = aws_api_gateway_method.options[0].http_method
-  status_code = aws_api_gateway_method_response.response[0].status_code
+  status_code = aws_api_gateway_method_response.options_response[0].status_code
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET,POST,PUT,DELETE,PATCH'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET,POST,PUT,DELETE'"
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-
-  response_templates = {
-    "application/json" = ""
   }
 }
