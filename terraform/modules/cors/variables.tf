4781e6bfbc06bf550a82cf787a5470fbc2cbde52
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-15 16:09:17 +0530

    Update variables.tf

--- terraform/modules/cors/variables.tf
+++ terraform/modules/cors/variables.tf
@@ -5,3 +5,9 @@ variable "rest_api_id" {
 variable "resource_id" {
   type = string
 }
+
+variable "create_options_method" {
+  description = "Whether to create the OPTIONS method for CORS"
+  type        = bool
+  default     = true
+}
