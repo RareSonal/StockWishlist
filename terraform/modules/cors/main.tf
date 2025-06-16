5016720af8fa313c91e028a2a328a994b4ebc9a1
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-16 18:28:39 +0530

    Update main.tf

--- terraform/modules/cors/main.tf
+++ terraform/modules/cors/main.tf
@@ -77,6 +77,8 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
   }
 
   depends_on = [
-    aws_api_gateway_method_response.options_response
+    aws_api_gateway_method_response.options_response,
+    aws_api_gateway_integration.options_mock  
   ]
 }
+
