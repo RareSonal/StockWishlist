8532e9467a788bd3f8683b9931acdf880629ebd6
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-16 16:47:39 +0530

    Update outputs.tf

--- terraform/modules/api_gateway/outputs.tf
+++ terraform/modules/api_gateway/outputs.tf
@@ -1,23 +1,39 @@
 output "api_id" {
-  value = aws_api_gateway_rest_api.api.id
+  description = "The ID of the API Gateway REST API"
+  value       = aws_api_gateway_rest_api.api.id
 }
 
 output "api_url" {
-  value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.region}.amazonaws.com/${var.stage_name}"
+  description = "Base invoke URL of the deployed API"
+  value       = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.region}.amazonaws.com/${var.stage_name}"
 }
 
 output "proxy_resource_id" {
-  value = aws_api_gateway_resource.v1_proxy.id
+  description = "Resource ID for /v1/{proxy+}"
+  value       = aws_api_gateway_resource.v1_proxy.id
 }
 
 output "login_resource_id" {
-  value = aws_api_gateway_resource.login.id
+  description = "Resource ID for /v1/login"
+  value       = aws_api_gateway_resource.login.id
 }
 
 output "stocks_resource_id" {
-  value = aws_api_gateway_resource.stocks.id
+  description = "Resource ID for /v1/stocks"
+  value       = aws_api_gateway_resource.stocks.id
 }
 
 output "wishlist_resource_id" {
-  value = aws_api_gateway_resource.wishlist.id
+  description = "Resource ID for /v1/wishlist"
+  value       = aws_api_gateway_resource.wishlist.id
+}
+
+output "deployment_id" {
+  description = "Deployment ID of the API Gateway"
+  value       = aws_api_gateway_deployment.api_deployment.id
+}
+
+output "stage_name" {
+  description = "Stage name used in deployment"
+  value       = aws_api_gateway_stage.api_stage.stage_name
 }
