dd7b462de363767980ad5d00323289e58de66bb9
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-19 16:13:25 +0530

    Update main.tf

--- terraform/modules/cognito/main.tf
+++ terraform/modules/cognito/main.tf
@@ -16,10 +16,24 @@ resource "aws_cognito_user_pool_client" "user_pool_client" {
     "ALLOW_USER_PASSWORD_AUTH",
     "ALLOW_ADMIN_USER_PASSWORD_AUTH",
     "ALLOW_REFRESH_TOKEN_AUTH",
-    "ALLOW_USER_SRP_AUTH"
+    "ALLOW_USER_SRP_AUTH",
+    "ALLOW_CUSTOM_AUTH",
+    "ALLOW_USER_AUTH"
   ]
 
   prevent_user_existence_errors = "LEGACY"
+
+  access_token_validity  = 60    
+  id_token_validity      = 60    
+  refresh_token_validity = 30    
+
+  token_validity_units {
+    access_token  = "minutes"
+    id_token      = "minutes"
+    refresh_token = "days"
+  }
+
+  auth_session_validity = 3 
 }
 
 data "aws_caller_identity" "current" {}
