c1dcf04baeb2986d2078929156b9e2a838a9e229
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-19 18:10:35 +0530

    Update seed_db.py

--- backend/seed_db_lambda/seed_db.py
+++ backend/seed_db_lambda/seed_db.py
@@ -10,7 +10,7 @@ sys.path.append('/opt')
 lambda_client = boto3.client('lambda')
 LAMBDA_FUNCTION_NAME = os.environ.get('AWS_LAMBDA_FUNCTION_NAME')
 
-DB_NAME = "stockwishlist"
+DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 DB_HOST = os.environ.get("DB_HOST")
