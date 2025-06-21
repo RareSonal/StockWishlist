5b0ebce1ff01b4d36f55beb7da48707d31802cc8
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-21 20:40:48 +0530

    Update variables.tf

--- terraform/modules/api_gateway/variables.tf
+++ terraform/modules/api_gateway/variables.tf
@@ -35,8 +35,4 @@ variable "cors_integration_ids" {
   default     = []
 }
 
-variable "include_cors" {
-  description = "Flag to include CORS integrations as deployment triggers"
-  type        = bool
-  default     = false
-}
+
