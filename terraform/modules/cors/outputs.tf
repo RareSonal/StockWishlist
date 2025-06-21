1636e0569e2e7e9d66accec45cf7a42db0c1dc2e
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-21 23:07:22 +0530

    Update outputs.tf

--- terraform/modules/cors/outputs.tf
+++ terraform/modules/cors/outputs.tf
@@ -1,3 +1,8 @@
 output "options_integration_id" {
   value = aws_api_gateway_integration_response.options_integration_response[0].id
 }
+
+output "method_id" {
+  value = try(aws_api_gateway_method.options[0].id, null)
+}
+
