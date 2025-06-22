ee51a94f5708eb89040e6fb5e6813295a5f272e4
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-22 18:51:19 +0530

    Update main.tf

--- terraform/modules/cors/main.tf
+++ terraform/modules/cors/main.tf
@@ -1,16 +1,13 @@
+# Add OPTIONS method for CORS
 resource "aws_api_gateway_method" "options" {
   count         = var.create_options_method ? 1 : 0
   rest_api_id   = var.rest_api_id
   resource_id   = var.resource_id
   http_method   = "OPTIONS"
   authorization = "NONE"
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
-resource "aws_api_gateway_integration" "options_mock" {
+resource "aws_api_gateway_integration" "options" {
   count                   = var.create_options_method ? 1 : 0
   rest_api_id             = var.rest_api_id
   resource_id             = var.resource_id
@@ -19,66 +16,46 @@ resource "aws_api_gateway_integration" "options_mock" {
   integration_http_method = "OPTIONS"
 
   request_templates = {
-    "application/json" = "{\"statusCode\": 200}"
+    "application/json" = <<EOF
+{
+  "statusCode": 200
+}
+EOF
   }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  depends_on = [
-    aws_api_gateway_method.options
-  ]
 }
 
-resource "aws_api_gateway_method_response" "options_response" {
-  count       = var.create_options_method ? 1 : 0
-  rest_api_id = var.rest_api_id
-  resource_id = var.resource_id
-  http_method = aws_api_gateway_method.options[0].http_method
-  status_code = "200"
+resource "aws_api_gateway_method_response" "options" {
+  count          = var.create_options_method ? 1 : 0
+  rest_api_id    = var.rest_api_id
+  resource_id    = var.resource_id
+  http_method    = aws_api_gateway_method.options[0].http_method
+  status_code    = "200"
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
     "method.response.header.Access-Control-Allow-Origin"  = true
-    "method.response.header.Access-Control-Max-Age"       = true
   }
 
   response_models = {
     "application/json" = "Empty"
   }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  depends_on = [
-    aws_api_gateway_integration.options_mock
-  ]
 }
 
-resource "aws_api_gateway_integration_response" "options_integration_response" {
+resource "aws_api_gateway_integration_response" "options" {
   count       = var.create_options_method ? 1 : 0
   rest_api_id = var.rest_api_id
   resource_id = var.resource_id
   http_method = aws_api_gateway_method.options[0].http_method
-  status_code = aws_api_gateway_method_response.options_response[0].status_code
+  status_code = aws_api_gateway_method_response.options[0].status_code
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET,POST,PUT,DELETE'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS,DELETE,PUT'",
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-    "method.response.header.Access-Control-Max-Age"       = "'3600'"
   }
 
-  lifecycle {
-    create_before_destroy = true
+  response_templates = {
+    "application/json" = ""
   }
-
-  depends_on = [
-    aws_api_gateway_method_response.options_response,
-    aws_api_gateway_integration.options_mock  
-  ]
 }
-
