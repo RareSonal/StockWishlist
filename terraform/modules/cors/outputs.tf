d91d8ba623a6d8f141dc8570d63ade8d72bad21a
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-22 19:26:20 +0530

    Update outputs.tf

--- terraform/modules/cors/outputs.tf
+++ terraform/modules/cors/outputs.tf
@@ -2,7 +2,7 @@
 
 output "options_integration_response_id" {
   description = "ID of the integration response for CORS OPTIONS method"
-  value       = try(aws_api_gateway_integration_response.options_integration_response[0].id, null)
+  value       = try(aws_api_gateway_integration_response.options[0].id, null)
 }
 
 output "method_id" {
