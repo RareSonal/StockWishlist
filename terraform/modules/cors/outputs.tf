b639c2f7b52fe14bde837a777a6ca12b84f47c79
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-22 18:54:54 +0530

    Update outputs.tf

--- terraform/modules/cors/outputs.tf
+++ terraform/modules/cors/outputs.tf
@@ -1,8 +1,10 @@
+
 output "options_integration_id" {
-  value = aws_api_gateway_integration_response.options_integration_response[0].id
+  description = "Integration ID for the OPTIONS method"
+  value       = try(aws_api_gateway_integration.options_mock[0].id, null)
 }
 
 output "method_id" {
-  value = try(aws_api_gateway_method.options[0].id, null)
+  description = "Method ID for the OPTIONS method"
+  value       = try(aws_api_gateway_method.options[0].id, null)
 }
-
