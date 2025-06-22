13c8b54c2af6b0b4410358bcaffc662d47053cbd
RareSonal <sonali.mandrupkar@gmail.com>
2025-06-22 20:53:49 +0530

    Update outputs.tf

--- terraform/modules/cors/outputs.tf
+++ terraform/modules/cors/outputs.tf
@@ -1,11 +1,9 @@
-
-
 output "options_integration_response_id" {
   description = "ID of the integration response for CORS OPTIONS method"
-  value       = try(aws_api_gateway_integration_response.options[0].id, null)
+  value       = length(aws_api_gateway_integration_response.options) > 0 ? aws_api_gateway_integration_response.options[0].id : ""
 }
 
 output "method_id" {
   description = "ID of the CORS OPTIONS method (if created)"
-  value       = try(aws_api_gateway_method.options[0].id, null)
+  value       = length(aws_api_gateway_method.options) > 0 ? aws_api_gateway_method.options[0].id : ""
 }
